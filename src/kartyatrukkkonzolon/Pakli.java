4b9485022883c5b15fe06ebb1c4d8e19d61a86a0
veres.marcell <veres.marcell@host.docker.internal>
2023-09-18 15:34:53 +0200

    órai munka

--- src/kartyatrukkkonzolon/Pakli.java
+++ src/kartyatrukkkonzolon/Pakli.java
@@ -4,22 +4,59 @@ public class Pakli {
     private Lap[] lapok;
     
     Pakli(){
-        
+        lapok=new Lap[22];
+        feltolt();
     }
     
     private void feltolt(){
-        
+        String[] szinek = {"P", "T", "Z", "M"};
+        String[] ertekek = {"Ász", "Kir", "Fel", "X", "IX", "VIII"};
+        int i = 1;
+        for (String szin : szinek) {
+            for (int e = 0; e < ertekek.length && i < lapok.length; e++) {
+                lapok[i++] = new Lap(szin + "_" + ertekek[e]);
+            }
+        }
     }
     
-    private void kirak(){
-        
+    void kirak(){
+        for (int i = 1; i < lapok.length; i++) {
+            System.out.printf("%-8s", lapok[i].getLeiras());
+            if (i % 3 == 0) {
+                System.out.println("");
+            }
+        }
     }
     
-    private void kever(int oszlop){
-        
+    void kever(int oszlop){
+        Lap[] ujPakli = new Lap[22];
+        switch (oszlop) {
+            case 1:
+                for (int i = 1; i <= 7; i++) {
+                    ujPakli[i] = lapok[20 - (i - 1) * 3];
+                    ujPakli[i + 7] = lapok[19 - (i - 1) * 3];
+                    ujPakli[i + 14] = lapok[21 - (i - 1) * 3];
+                }
+                break;
+            case 2:
+                for (int i = 1; i <= 7; i++) {
+                    ujPakli[i] = lapok[19 - (i - 1) * 3];
+                    ujPakli[i + 7] = lapok[20 - (i - 1) * 3];
+                    ujPakli[i + 14] = lapok[21 - (i - 1) * 3];
+                }
+                break;
+            case 3:
+                for (int i = 1; i <= 7; i++) {
+                    ujPakli[i] = lapok[19 - (i - 1) * 3];
+                    ujPakli[i + 7] = lapok[21 - (i - 1) * 3];
+                    ujPakli[i + 14] = lapok[20 - (i - 1) * 3];
+                }
+                break;
+        }
+        lapok = ujPakli;
     }
     
-    private void ezvolt(){
-        
+    void ezVolt(){
+        System.out.println("A választott lap: " + lapok[11].getLeiras());
     }
 }
