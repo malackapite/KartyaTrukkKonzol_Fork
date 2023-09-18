fec96ec08349b2559d48805d0b3420b8126e468d
veres.marcell <veres.marcell@host.docker.internal>
2023-09-18 15:15:31 +0200

    órai munka

---
+++ src/kartyatrukkkonzolon/Pakli.java
@@ -0,0 +1,25 @@
+package kartyatrukkkonzolon;
+
+public class Pakli {
+    private Lap[] lapok;
+    
+    Pakli(){
+        
+    }
+    
+    private void feltolt(){
+        
+    }
+    
+    private void kirak(){
+        
+    }
+    
+    private void kever(int oszlop){
+        
+    }
+    
+    private void ezvolt(){
+        
+    }
+}
